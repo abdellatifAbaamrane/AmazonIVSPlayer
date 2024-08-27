cf6413c15ab26f6fd1be66c83ab16c4e39efe086
abdellatifAbaamrane <135992498+abdellatifAbaamrane@users.noreply.github.com>
2024-08-27 15:04:20 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,24 +6,17 @@ import PackageDescription
 let package = Package(
     name: "AmazonIVSPlayer",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AmazonIVSPlayer",
-            targets: ["AmazonIVSPlayer"]),
+            targets: ["AmazonIVSPlayer"]
+        )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "AmazonIVSPlayer",
-            url: "https://player.live-video.net/1.31.0/AmazonIVSPlayer.xcframework.zip",
+            url: "https://github.com/loopsocial/AmazonIVSPlayer/releases/download/v1.31.0/AmazonIVSPlayer.xcframework.zip",
             checksum: "6b1dbfe41258c367ae0c2f2c52a6886f5082232208e3a93d7ae3f77bec7e8ae5"
-        ),
-
-
+        )
     ]
 )
