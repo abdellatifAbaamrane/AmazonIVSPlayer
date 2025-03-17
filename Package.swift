3a5bba189885646f37621b961999998eeabdde7a
abdellatifAbaamrane <135992498+abdellatifAbaamrane@users.noreply.github.com>
2025-03-17 22:14:44 +0000

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AmazonIVSPlayer",
+    products: [
+        .library(
+            name: "AmazonIVSPlayer",
+            targets: ["AmazonIVSPlayer"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "AmazonIVSPlayer",
+            url: "https://github.com/loopsocial/AmazonIVSPlayer/releases/download/v1.31.0/AmazonIVSPlayer.xcframework.zip",
+            checksum: "6b1dbfe41258c367ae0c2f2c52a6886f5082232208e3a93d7ae3f77bec7e8ae5"
+        )
+    ]
+)
